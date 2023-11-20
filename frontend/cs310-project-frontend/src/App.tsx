5972e0dbe19a73c389273a84127b44ca3c9b14f5
UBC Student <stdntyx@students.cs.ubc.ca>
2023-11-20 12:13:02 -0800

    merged with main

--- frontend/cs310-project-frontend/src/App.tsx
+++ frontend/cs310-project-frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 
